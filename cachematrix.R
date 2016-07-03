1f6f48e6ad60dd87ff5ed69c5e619d56a7396831
vinay.varma <ramavarma@gmail.com>
2016-07-03 21:58:20 +1200

    Initial Version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix function creates a special "matrix" object that can cache its inverse.
+## The functon has following getter and setter methods to set the value
+## setMatrix: Sets the Matrix
+## getMatrix: Gets the Matrix that has been set
+## setInverse: Sets the inverse of Matrix
+## getInverse: Gets the inverse of the Matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
+      invrse <- NULL
+      setMatrix <- function(y){
+            message("Set Matrix called")
+            x <<- y
+            invrse <<- NULL
+      }
+      
+      getMatrix <- function(){
+            message("Get Matrix called")
+            x
+      }
+      
+      setInverse <- function(inverse) {
+            message("Set Inverse called")
+            invrse <<- inverse
+            return(inverse)
+      }
+      getInverse <- function() {
+            message("Get Inverse called")
+            invrse     
+      }
+      
+      list(setMatrix = setMatrix,
+           getMatrix = getMatrix,
+           setInverse = setInverse,
+           getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## The following function cacheSolve calculates the inverse of a inversable matrix.
+## If the inverse has been already calculated, then it would get the cached
+## value and will skip any further processing
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      invrse <- x$getInverse()
+
+      if (!is.null(invrse)){
+            message("Inverse available, providing cached data")
+            return(invrse)
+      }
+      message("Inverse not available, hence calculating inverse")
+      invMat <-x$getMatrix()
+      invrse <- solve(invMat)
+      x$setInverse(invrse)
+      invrse
 }
+
